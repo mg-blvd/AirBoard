565d64046c52162a7798a375ae76a33b44672559
MoniBarrios <49417323+MoniBarrios@users.noreply.github.com>
2019-05-11 19:02:07 -0700

    ..

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import sys
 from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout,
-                            QHBoxLayout, QPushButton, QComboBox, QLineEdit)
-from PyQt5.QtCore import pyqtSlot
+                            QHBoxLayout, QPushButton, QComboBox, QLineEdit, QSlider)
+from PyQt5.QtCore import pyqtSlot, Qt
 from paint import DrawingWindow
 import numpy as np
 import cv2
+import threading
 import speech as VoiceRecord
 from collections import deque
+from pygame import mixer
 
-
-from pygame import mixer # Load the required library
 
 
 
@@ -18,120 +18,125 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
         mixer.init()
+        self.setStyleSheet(open('css/style.css').read())
 
         self.our_window = DrawingWindow()
         self.voiceObject = VoiceRecord.VoiceRecord();
 
-        #Window Title
+        # Window Title
         self.setWindowTitle("Home Page")
+        self.drawing_message = QLabel("These are the coloring options:")
+        self.other_message = QLabel("These are all the other options:")
 
-        #Welcome Message
+        #Thread for voice commands
+        self.voice_thread = threading.Thread(target=self.voice_control)
+
+        # Welcome Message
         self.welcome = QLabel("Welcome!!\nClick on the button below to access the app.")
+        self.welcome.setAlignment(Qt.AlignCenter)
         self.color_message = QLabel("Change the color of the line.")
 
 
-        #Button that takes you to the app.
+        # Button that takes you to the app.
         self.app_button = QPushButton("Start Drawing!!")
         self.app_button.clicked.connect(self.on_click)
 
-        #Button that deleates clear_everything
+        # Button that deleates clear_everything
         self.clear_button = QPushButton("Clear the Screen")
         self.clear_button.clicked.connect(self.clean_screen)
 
 
-        #Button to listen for voice commands
+        # Button to listen for voice commands
         self.voice_button = QPushButton("Voice Command")
         self.voice_button.clicked.connect(self.voice_click)
 
-        #Button to listen for save
+        # Button to listen for save
         self.save_button1 = QPushButton("Save drawing")
         self.save_button1.clicked.connect(self.save_image1)
         self.save_button2 = QPushButton("Save drawing with background")
         self.save_button2.clicked.connect(self.save_image2)
 
 
-        #Colors Combobox
+        # Colors Combobox
         options = ["blue", "green", "red", "yellow"]
         self.choose_color = QComboBox()
         self.choose_color.addItems(options)
         self.choose_color.currentIndexChanged.connect(self.color_chosen)
 
+        # Slider for Brush Size
+        self.slider_name = QLabel("Brush Size: 2")
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(1)
+        self.slider.setMaximum(50)
+        self.slider.setValue(2)
+        self.slider.setFocusPolicy(Qt.StrongFocus)
+        self.slider.setTickPosition(QSlider.TicksBothSides)
+        self.slider.setTickInterval(10)
+        self.slider.setSingleStep(1)
+        self.slider.valueChanged.connect(self.on_slider_change)
 
-        #Window Setup
+        #Close windows Button
+        self.close_button = QPushButton("Close the Windows")
+        self.close_button.clicked.connect(self.close_the_wins)
+
+
+        #Create the coloring options section
+        self.coloring_options = QVBoxLayout()
+        self.coloring_options.addWidget(self.drawing_message)
+        self.coloring_options.addWidget(self.color_message)
+        self.coloring_options.addWidget(self.choose_color)
+        self.coloring_options.addWidget(self.slider_name)
+        self.coloring_options.addWidget(self.slider)
+        self.coloring_options.addWidget(self.clear_button)
+
+        #Create other options section
+        self.other_options = QVBoxLayout()
+        self.other_options.addWidget(self.other_message)
+        self.other_options.addWidget(self.voice_button)
+        self.other_options.addWidget(self.save_button1)
+        self.other_options.addWidget(self.save_button2)
+        self.other_options.addWidget(self.close_button)
+
+        #Put the two option squares side by side
+        self.hbox = QHBoxLayout()
+        self.hbox.addLayout(self.coloring_options)
+        self.hbox.addLayout(self.other_options)
+
+        # Window Setup
         self.vbox = QVBoxLayout()
         self.vbox.addWidget(self.welcome)
         self.vbox.addWidget(self.app_button)
-
-        self.vbox.addWidget(self.color_message)
-        self.vbox.addWidget(self.choose_color)
-        self.vbox.addWidget(self.clear_button)
-        self.vbox.addWidget(self.voice_button)
-        self.vbox.addWidget(self.save_button1)
-        self.vbox.addWidget(self.save_button2)
-
+        self.vbox.addLayout(self.hbox)
         self.setLayout(self.vbox)
 
     @pyqtSlot()
     def on_click(self):
-        print("Clicked!!")
-        self.our_window.clear_everything()
-        self.our_window.paintWindow = np.zeros((471,636,3)) + 255
+        self.our_window.close_wins = False
         self.our_window.camera = cv2.VideoCapture(0)
         self.our_window.draw()
 
+    @pyqtSlot()
+    def on_slider_change(self):
+        slider_value = self.slider.value()
+        self.slider_name.setText(f"Brush Size: {slider_value}")
+        self.our_window.setBrush(slider_value)
 
+    @pyqtSlot()
     def save_image1(self):
         self.our_window.save1()
-
-
+        mixer.music.load('audio/save.mp3')
+        mixer.music.play()
+    @pyqtSlot()
     def save_image2(self):
         self.our_window.save2()
-
-
-    def voice_click(self):
-        print("Voice Clicked!!")
-        text = self.voiceObject.send_text();
-
-        print(text)
-
-        if text.lower() == "change color to blue":
-            #self.our_window.colorIndex = 0;
-            self.choose_color.setCurrentIndex(0);
-            mixer.music.load('audio/blue.mp3')
-            mixer.music.play()
-
-
-
-        elif text.lower() == "change color to green":
-            #self.our_window.colorIndex = 1;
-            self.choose_color.setCurrentIndex(1);
-            mixer.music.load('audio/green.mp3')
-            mixer.music.play()
-
-
-        elif text.lower() == "change color to red":
-            #self.our_window.colorIndex = 2;
-            self.choose_color.setCurrentIndex(2);
-            mixer.music.load('audio/red.mp3')
-            mixer.music.play()
-
-        elif text.lower() == "change color to yellow":
-            #self.our_window.colorIndex = 3;
-            self.choose_color.setCurrentIndex(3);
-            mixer.music.load('audio/yellow.mp3')
-            mixer.music.play()
-
-        elif text.lower() == "clear screen":
-            self.our_window.clear_everything()
-            mixer.music.load('audio/erase.mp3')
-            mixer.music.play()
-
-    def clean_screen(self):
-        self.our_window.clear_everything()
-        mixer.music.load('audio/erase.mp3')
+        mixer.music.load('audio/save.mp3')
         mixer.music.play()
+    @pyqtSlot()
+    def voice_click(self):
+        self.voice_thread.start()
+        self.voice_thread = threading.Thread(target=self.voice_control)
 
-
+    @pyqtSlot()
     def color_chosen(self):
         new_color = self.choose_color.currentText()
         if(new_color == 'blue'):
@@ -145,10 +150,81 @@ class Window(QWidget):
         else:
             self.our_window.colorIndex = 3
 
+    def close_the_wins(self):
+        self.our_window.close_wins = True
+        self.our_window.clear_everything()
+
+    def voice_control(self):
+        print("Voice Button Clicked!!")
+        mixer.music.load('audio/bell.wav')
+        mixer.music.play()
+        text = self.voiceObject.send_text();
+        print(text)
+
+        text = text.lower()
+
+        if "blue" in text:
+            #self.our_window.colorIndex = 0;
+            self.choose_color.setCurrentIndex(0);
+            mixer.music.load('audio/blue.mp3')
+            mixer.music.play()
+
+        elif "green" in text:
+            #self.our_window.colorIndex = 1;
+            self.choose_color.setCurrentIndex(1);
+            mixer.music.load('audio/green.mp3')
+            mixer.music.play()
+
+        elif "red" in text:
+            #self.our_window.colorIndex = 2;
+            self.choose_color.setCurrentIndex(2);
+            mixer.music.load('audio/red.mp3')
+            mixer.music.play()
+
+        elif "yellow" in text:
+            #self.our_window.colorIndex = 3;
+            self.choose_color.setCurrentIndex(3);
+            mixer.music.load('audio/yellow.mp3')
+            mixer.music.play()
+
+        # Changing Brush Size: Say size and a number between 1 and 50 in the same sentence
+        elif "size" in text:
+            for substr in text.split(' '):
+                if substr.isdigit():
+                    new_size = int(substr)
+                    if new_size <= 50 and new_size > 0:
+                        self.slider.setValue(new_size)
+                        self.slider_name.setText("Brush Size: " + str(new_size))
+                        self.our_window.setBrush(new_size)
+                        mixer.music.load('audio/{}.mp3'.format(substr))
+                        mixer.music.play()
+                    else:
+                        print(substr + " is an invalid size")
+        # Clears all drawings
+        elif "clear" in text or "clean" in text:
+            self.our_window.clear_everything()
+            mixer.music.load('audio/erase.mp3')
+            mixer.music.play()
+
+        elif "close" in text:
+            self.close_the_wins()
+
+        elif "open" in text:
+            self.on_click()
+
+    @pyqtSlot()
+    def clean_screen(self):
+        self.our_window.clear_everything()
+        mixer.music.load('audio/erase.mp3')
+        mixer.music.play()
+
 
 
 
 app = QApplication(sys.argv)
 main = Window()
+p =  main.palette()
+p.setColor(main.backgroundRole(), Qt.black)
+main.setPalette(p)
 main.show()
 sys.exit(app.exec_())
--- paint.py
+++ paint.py
@@ -2,71 +2,110 @@ import numpy as np
 import cv2
 from collections import deque
 import uuid
+from PIL import Image
+
 
 class DrawingWindow():
     def __init__(self):
-        print("yep")
-        # Define the upper and lower boundaries for a color to be considered "Blue"
-        self.blueLower = np.array([100, 60, 60])
-        self.blueUpper = np.array([140, 255, 255])
+        #Flag that will allow is to close the windows
+        self.close_wins = False
+        # Define the upper and lower boundaries for a color to be considered "PinkishRed"
+        self.redLower = np.array([170,50,220])
+        self.redUpper = np.array([180,255,255])
 
-
+        # Define size of brush for drawing
+        self.brush_size = 2
 
         # Define a 5x5 kernel for erosion and dilation
         self.kernel = np.ones((5, 5), np.uint8)
 
-        # Setup deques to store separate colors in separate arrays
-        self.bpoints = [deque(maxlen=512)]
-        self.gpoints = [deque(maxlen=512)]
-        self.rpoints = [deque(maxlen=512)]
-        self.ypoints = [deque(maxlen=512)]
+        # List of layers for different point sizes and their colors
+        self.points = []
+        self.points_size = []
+        self.colorIndexes = []
 
-        self.bindex = 0
-        self.gindex = 0
-        self.rindex = 0
-        self.yindex = 0
-
+        # RGB values of Red, Blue, Green, and Yellow
         self.colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
         self.colorIndex = 0
 
-        # Setup the Paint interface
-        self.paintWindow = np.zeros((471,636,3)) + 255
-        print("Window Created")
 
+        self.width = 0
+        self.height = 0
 
         # Load the video
         self.camera = cv2.VideoCapture(0)
 
 
 
+
+        # Setup the Paint interface, values here not important, will change in the Draw function
+        self.paintWindow = np.zeros((self.width,self.height,3)) + 255
+
+
+
+
+
+
+
+
+
+
     def draw(self):
         cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
         # Keep looping
+
+
+
         while True:
             # Grab the current paintWindow
             (self.grabbed, self.frame) = self.camera.read()
             self.frame = cv2.flip(self.frame, 1)
             self.hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
 
+
+            #Need to do a funky save image and get the size because opencv has no easy way to get resolution of video stream. Use that size of the image to set the white frame window and then replace the image with a white screen because noone wants a mugshot of themself
+            if(self.width == 0):
+                cv2.imwrite("images/temp.jpg", self.frame)
+                im = Image.open("images/temp.jpg")
+                self.width, self.height = im.size
+                # Setup the Paint interface
+                self.paintWindow = np.zeros((self.height,self.width,3)) + 255
+                cv2.imwrite("images/temp.jpg", self.paintWindow)
+
+
+
             # Check to see if we have reached the end of the video
             if not self.grabbed:
                 break
 
-            # Determine which pixels fall within the blue boundaries and then blur the binary image
-            self.blueMask = cv2.inRange(self.hsv, self.blueLower, self.blueUpper)
-            self.blueMask = cv2.erode(self.blueMask, self.kernel, iterations=2)
-            self.blueMask = cv2.morphologyEx(self.blueMask, cv2.MORPH_OPEN, self.kernel)
-            self.blueMask = cv2.dilate(self.blueMask,self.kernel, iterations=1)
+            # Determine which pixels fall within the red boundaries and then blur the binary image
+            self.redMask = cv2.inRange(self.hsv, self.redLower, self.redUpper)
+            self.redMask = cv2.erode(self.redMask, self.kernel, iterations=2)
+            self.redMask = cv2.morphologyEx(self.redMask, cv2.MORPH_OPEN, self.kernel)
+            self.redMask = cv2.dilate(self.redMask,self.kernel, iterations=1)
 
             # Find contours in the image
-            (_, self.cnts, _) = cv2.findContours(self.blueMask.copy(), cv2.RETR_EXTERNAL,
-                                            cv2.CHAIN_APPROX_SIMPLE)
+
+            # IMPORTANT: Depending on your machine, you may need 2 or 3 result parameters
+            # for this function. Try (self.conts, _) or (_, self.conts, _) respectively if there is an issue.
+            try:
+                (_, self.cnts, _) = cv2.findContours(self.redMask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            except:
+                (self.cnts, _) = cv2.findContours(self.redMask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+
+
+
+
+
+
             self.center = None
 
+            self.setBrush(self.brush_size)
+            current_index = len(self.points) - 1
             # Check to see if any contours were found
             if len(self.cnts) > 0:
-                # Sort the contours and find the largest one -- we
-                # will assume this contour correspondes to the area of the bottle cap
+                # Sort the contours and find the largest one
                 self.cnt = sorted(self.cnts, key = cv2.contourArea, reverse = True)[0]
                 # Get the radius of the enclosing circle around the found contour
                 ((self.x, self.y), self.radius) = cv2.minEnclosingCircle(self.cnt)
@@ -77,52 +116,60 @@ class DrawingWindow():
                 self.center = (int(self.M['m10'] / self.M['m00']), int(self.M['m01'] / self.M['m00']))
 
                 if self.colorIndex == 0:
-                    self.bpoints[self.bindex].appendleft(self.center)
+                    self.points[current_index][0][self.colorIndexes[current_index][0]].appendleft(self.center)
                 elif self.colorIndex == 1:
-                    self.gpoints[self.gindex].appendleft(self.center)
+                    self.points[current_index][1][self.colorIndexes[current_index][1]].appendleft(self.center)
                 elif self.colorIndex == 2:
-                    self.rpoints[self.rindex].appendleft(self.center)
+                    self.points[current_index][2][self.colorIndexes[current_index][2]].appendleft(self.center)
                 elif self.colorIndex == 3:
-                    self.ypoints[self.yindex].appendleft(self.center)
-            # Append the next deque when no contours are detected (i.e., bottle cap reversed)
+                    self.points[current_index][3][self.colorIndexes[current_index][3]].appendleft(self.center)
+            # Append the next deque when no contours are detected
             else:
-                self.bpoints.append(deque(maxlen=512))
-                self.bindex += 1
-                self.gpoints.append(deque(maxlen=512))
-                self.gindex += 1
-                self.rpoints.append(deque(maxlen=512))
-                self.rindex += 1
-                self.ypoints.append(deque(maxlen=512))
-                self.yindex += 1
+                self.points[current_index][0].append(deque(maxlen=512))
+                self.colorIndexes[current_index][0] += 1
+
+                self.points[current_index][1].append(deque(maxlen=512))
+                self.colorIndexes[current_index][1] += 1
+
+                self.points[current_index][2].append(deque(maxlen=512))
+                self.colorIndexes[current_index][2] += 1
+
+                self.points[current_index][3].append(deque(maxlen=512))
+                self.colorIndexes[current_index][3] += 1
 
 
-            # Draw lines of all the colors (Blue, Green, Red and Yellow)
-            self.points = [self.bpoints, self.gpoints, self.rpoints, self.ypoints]
-            for i in range(len(self.points)):
-                for j in range(len(self.points[i])):
-                    for k in range(1, len(self.points[i][j])):
-                        if self.points[i][j][k - 1] is None or self.points[i][j][k] is None:
-                            continue
-                        cv2.line(self.frame, self.points[i][j][k - 1], self.points[i][j][k], self.colors[i], 2)
-                        cv2.line(self.paintWindow, self.points[i][j][k - 1], self.points[i][j][k], self.colors[i], 2)
+            # Draw lines of all the colors (Blue, Green, Red and Yellow), as well as lines of different sizes
+
+            for l in range(len(self.points)):
+                for i in range(len(self.points[l])):
+                    for j in range(len(self.points[l][i])):
+                        for k in range(1, len(self.points[l][i][j])):
+                            if self.points[l][i][j][k - 1] is None or self.points[l][i][j][k] is None:
+                                continue
+                            cv2.line(self.frame, self.points[l][i][j][k - 1], self.points[l][i][j][k], self.colors[i], self.points_size[l])
+                            cv2.line(self.paintWindow, self.points[l][i][j][k - 1], self.points[l][i][j][k], self.colors[i], self.points_size[l])
 
             # Show the frame and the paintWindow image
             cv2.imshow("Tracking", self.frame)
 
 
-            cv2.imshow("Paint", self.paintWindow)
-
             # If the 'q' key is pressed, stop the loop
-            if cv2.waitKey(1) & 0xFF == ord("q"):
+            if cv2.waitKey(1) & self.close_wins:
                 break
+
+
 
         # Cleanup the camera and close any open windows
         self.camera.release()
         cv2.destroyAllWindows()
-        print("nope")
 
-
-
+    def setBrush(self, new_size):
+        # If the brush size has changed or there are no layers: add a new layer
+        if self.brush_size != new_size or not self.points:
+            self.brush_size = new_size
+            self.points.append([[deque(maxlen=512)], [deque(maxlen=512)], [deque(maxlen=512)], [deque(maxlen=512)]])
+            self.points_size.append(self.brush_size)
+            self.colorIndexes.append([0,0,0,0])
 
     def save1(self):
         cv2.imwrite("images/image%d.jpg" % uuid.uuid4(), self.paintWindow)
@@ -131,15 +178,8 @@ class DrawingWindow():
         cv2.imwrite("images/image%d.jpg" % uuid.uuid4(), self.frame)
 
     def clear_everything(self):
-        #erase all
-        self.bpoints = [deque(maxlen=512)]
-        self.gpoints = [deque(maxlen=512)]
-        self.rpoints = [deque(maxlen=512)]
-        self.ypoints = [deque(maxlen=512)]
-
-        self.bindex = 0
-        self.gindex = 0
-        self.rindex = 0
-        self.yindex = 0
-
+        #erase all - clears all layers
+        self.points.clear()
+        self.colorIndexes.clear()
+        self.points_size.clear()
         self.paintWindow[67:,:,:] = 255
